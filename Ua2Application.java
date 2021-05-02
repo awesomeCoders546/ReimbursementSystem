bb40f40e7d762b914fa7b4579da2db5e81c4f408
awesomeCoders546 <82926007+awesomeCoders546@users.noreply.github.com>
2021-05-02 12:46:49 +0530

    Add files via upload

---
+++ Ua2Application.java
@@ -0,0 +1,14 @@
+package com.sjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Ua2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ua2Application.class, args);
+	}
+
+}
